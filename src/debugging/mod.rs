cf1e5c6fc02bfcfd41158c4043169e36cc80486b
Amy Gilhespy <amygilhespy@protonmail.com>
2026-06-20 14:10:27 -0500

    Fixed the debugging macros.

--- src/debugging/mod.rs
+++ src/debugging/mod.rs
@@ -1,93 +1,114 @@
 #[macro_export]
 macro_rules! info {
 	($($arg:tt)*) => {
-		{
-			let (f, l, c) = (file!(), line!(), column!());
-			let m = ::std::format!("[{f}:{l}:{c}]: {}", ::std::format!($($arg)*).as_str());
-			#[cfg(feature = "backtrace")]
-			let m = ::std::format!("{m}\n=== BACKTRACE ===\n{:?}", backtrace::Backtrace::new());
-			#[cfg(feature = "stderr")]
-			eprintln!("{m}");
-			#[cfg(feature = "stdout")]
-			println!("{m}");
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::print(m.as_str());
-			let _ = m;
-		}
+			emit_info(file!(), line!(), column!(), ::std::format!($($arg)*));
 	};
 }
 
 #[macro_export]
 macro_rules! warn {
 	($($arg:tt)*) => {
-		{
-			let (f, l, c) = (file!(), line!(), column!());
-			let m = ::std::format!("[{f}:{l}:{c}]: {}", ::std::format!($($arg)*).as_str());
-			#[cfg(feature = "backtrace")]
-			let m = ::std::format!("{m}\n=== BACKTRACE ===\n{:?}", backtrace::Backtrace::new());
-			#[cfg(feature = "stderr")]
-			eprintln!("{m}");
-			#[cfg(feature = "stdout")]
-			println!("{m}");
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::push_warning(m.as_str());
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::print(m.as_str());
-			let _ = m;
-		}
+			emit_warn(file!(), line!(), column!(), ::std::format!($($arg)*));
 	};
 }
 
 #[macro_export]
 macro_rules! error {
 	($($arg:tt)*) => {
-		{
-			let (f, l, c) = (file!(), line!(), column!());
-			let m = ::std::format!("[{f}:{l}:{c}]: {}", ::std::format!($($arg)*).as_str());
-			#[cfg(feature = "backtrace")]
-			let m = ::std::format!("{m}\n=== BACKTRACE ===\n{:?}", backtrace::Backtrace::new());
-			#[cfg(feature = "stderr")]
-			eprintln!("{m}");
-			#[cfg(feature = "stdout")]
-			println!("{m}");
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::push_error(m.as_str());
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::print(m.as_str());
-			let _ = m;
-		}
+			emit_error(file!(), line!(), column!(), ::std::format!($($arg)*));
 	};
 }
 
 #[macro_export]
 macro_rules! fatal {
 	($($arg:tt)*) => {
-		{
-			let (f, l, c) = (file!(), line!(), column!());
-			let m = ::std::format!("[{f}:{l}:{c}]: {}", ::std::format!($($arg)*).as_str());
-			#[cfg(feature = "backtrace")]
-			let m = ::std::format!("{m}\n=== BACKTRACE ===\n{:?}", backtrace::Backtrace::new());
-			#[cfg(feature = "stderr")]
-			eprintln!("{m}");
-			#[cfg(feature = "stdout")]
-			println!("{m}");
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::push_error(m.as_str());
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::print(m.as_str());
-			#[cfg(not(test))]
-			#[cfg(feature = "godot")]
-			$crate::godot::quit(1);
-			::std::panic!("{m}");
-		}
+			emit_fatal(file!(), line!(), column!(), ::std::format!($($arg)*));
 	};
+}
+
+#[allow(clippy::needless_pass_by_value)]
+pub fn emit_info(file: &str, line: u32, column: u32, message: String) {
+	let message = ::std::format!("[{file}:{line}:{column}]: {message}");
+	#[cfg(feature = "backtrace")]
+	let message = ::std::format!(
+		"{message}\n=== BACKTRACE ===\n{:?}",
+		backtrace::Backtrace::new()
+	);
+	#[cfg(feature = "stderr")]
+	eprintln!("{message}");
+	#[cfg(feature = "stdout")]
+	println!("{message}");
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::print(message.as_str());
+	let _ = message;
+}
+
+#[allow(clippy::needless_pass_by_value)]
+pub fn emit_warn(file: &str, line: u32, column: u32, message: String) {
+	let message = ::std::format!("[{file}:{line}:{column}]: {message}");
+	#[cfg(feature = "backtrace")]
+	let message = ::std::format!(
+		"{message}\n=== BACKTRACE ===\n{:?}",
+		backtrace::Backtrace::new()
+	);
+	#[cfg(feature = "stderr")]
+	eprintln!("{message}");
+	#[cfg(feature = "stdout")]
+	println!("{message}");
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::push_warning(message.as_str());
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::print(message.as_str());
+	let _ = message;
+}
+
+#[allow(clippy::needless_pass_by_value)]
+pub fn emit_error(file: &str, line: u32, column: u32, message: String) {
+	let message = ::std::format!("[{file}:{line}:{column}]: {message}");
+	#[cfg(feature = "backtrace")]
+	let message = ::std::format!(
+		"{message}\n=== BACKTRACE ===\n{:?}",
+		backtrace::Backtrace::new()
+	);
+	#[cfg(feature = "stderr")]
+	eprintln!("{message}");
+	#[cfg(feature = "stdout")]
+	println!("{message}");
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::push_error(message.as_str());
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::print(message.as_str());
+	let _ = message;
+}
+
+/// # Panics
+#[allow(clippy::needless_pass_by_value)]
+pub fn emit_fatal(file: &str, line: u32, column: u32, message: String) {
+	let message = ::std::format!("[{file}:{line}:{column}]: {message}");
+	#[cfg(feature = "backtrace")]
+	let message = ::std::format!(
+		"{message}\n=== BACKTRACE ===\n{:?}",
+		backtrace::Backtrace::new()
+	);
+	#[cfg(feature = "stderr")]
+	eprintln!("{message}");
+	#[cfg(feature = "stdout")]
+	println!("{message}");
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::push_error(message.as_str());
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::print(message.as_str());
+	#[cfg(not(test))]
+	#[cfg(feature = "godot")]
+	crate::godot::quit(1);
+	::std::panic!("{message}");
 }
 
 #[cfg(test)]
