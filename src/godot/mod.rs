8ad6a772d511e6cf968458dec0f2b30219c0b5be
Amy Gilhespy <amygilhespy@protonmail.com>
2026-06-28 13:53:02 -0500

    Updated godot module.

--- src/godot/mod.rs
+++ src/godot/mod.rs
@@ -1,20 +1,8 @@
 use godot::{
-	classes::{Engine, SceneTree, Window},
-	meta::ToGodot,
-	obj::{Gd, Singleton},
+	classes::{Engine, Node, SceneTree, Window, class_macros::private::virtuals::Os::NodePath},
+	meta::{AsArg, ToGodot},
+	obj::{Gd, Inherits, Singleton},
 };
-
-#[must_use]
-pub fn get_scene_root() -> Option<Gd<Window>> {
-	get_scene_tree().and_then(|s| s.get_root())
-}
-
-#[must_use]
-pub fn get_scene_tree() -> Option<Gd<SceneTree>> {
-	Engine::singleton()
-		.get_main_loop()
-		.and_then(|m| m.try_cast::<SceneTree>().ok())
-}
 
 pub fn push_error(message: &str) {
 	godot::global::push_error(&[message.to_variant()]);
@@ -33,7 +21,27 @@ pub fn print_rich(message: &str) {
 }
 
 pub fn quit(exit_code: i32) {
-	if let Some(mut scene_tree) = get_scene_tree() {
+	if let Some(mut scene_tree) = try_get_scene_tree() {
 		scene_tree.quit_ex().exit_code(exit_code).done();
 	}
+}
+
+#[must_use]
+pub fn try_get_rooted_node_as<T>(path: impl AsArg<NodePath>) -> Option<Gd<T>>
+where
+	T: Inherits<Node>,
+{
+	try_get_scene_root().and_then(|r| r.try_get_node_as::<T>(path))
+}
+
+#[must_use]
+pub fn try_get_scene_root() -> Option<Gd<Window>> {
+	try_get_scene_tree().and_then(|s| s.get_root())
+}
+
+#[must_use]
+pub fn try_get_scene_tree() -> Option<Gd<SceneTree>> {
+	Engine::singleton()
+		.get_main_loop()
+		.and_then(|m| m.try_cast::<SceneTree>().ok())
 }
